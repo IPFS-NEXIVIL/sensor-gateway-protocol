873a6762faeddd4932b5682049d0e37f66592dd4
liradb2000 <liradb2000@live.co.kr>
2023-11-21 23:41:19 +0900

    For expo;

--- src/main.rs
+++ src/main.rs
@@ -1,140 +1,167 @@
 mod ble;
 mod wifi;
-use ble::btlescan;
+// use ble::btlescan;
 use chrono::Utc;
-use futures::FutureExt;
-use rustyline_async::{Readline, ReadlineEvent, SharedWriter};
+// use futures::FutureExt;
+// use rustyline_async::{Readline, ReadlineEvent, SharedWriter};
 use std::net::SocketAddr;
 use std::{
     io::{ErrorKind, Write},
     sync::Arc,
     time::Duration,
 };
-use tokio::fs::{File, OpenOptions};
+use tokio::fs::OpenOptions;
 use tokio::io::AsyncWriteExt;
 use tokio::net::UdpSocket;
+// use tokio::sync::mpsc::UnboundedSender;
 use tokio::sync::{mpsc, Notify};
 use tokio::time::sleep;
-use wifi::startWifi;
+// use wifi::startWifi;
+use tokio::signal;
+
+use clap::Parser;
+use regex::Regex;
+// use rand::{rngs::StdRng, Rng, SeedableRng};
+
 // enum Command {
 //     "getPorts"
 // }
 
+#[derive(Debug, Parser)]
+#[clap(name = "test-serial")]
+struct Opt {
+    #[clap(long)]
+    port: String,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let cancel = Arc::new(Notify::new());
-    let (mut rl, mut stdout) = Readline::new(format!("$"))?;
+    let opt = Opt::parse();
 
-    fn get_ports(mut stdout: SharedWriter) {
-        let ports = serialport::available_ports().expect("No ports found!");
-        for p in ports {
-            writeln!(stdout, "{}", p.port_name).unwrap();
-        }
-    }
-    async fn open_port(
-        mut stdout: SharedWriter,
-        cancel: Arc<Notify>,
-        name: &str,
-        baudrate: u32,
-    ) -> anyhow::Result<()> {
-        let listener = UdpSocket::bind("0.0.0.0:0".parse::<SocketAddr>().unwrap()).await?;
-        let (tx, mut rx) = mpsc::unbounded_channel::<String>();
-        let noti = Arc::new(Notify::new());
-        let (tx_done, mut rx_done) = mpsc::unbounded_channel::<String>();
+    let listener = UdpSocket::bind("0.0.0.0:0".parse::<SocketAddr>().unwrap()).await?;
+    let (tx, mut rx) = mpsc::unbounded_channel::<String>();
+    let noti = Arc::new(Notify::new());
+    let (tx_done, mut rx_done) = mpsc::unbounded_channel::<String>();
 
-        let tstring = [
-            "<21020001,REQ,1234>".to_ascii_uppercase(),
-            "<22020001,REQ,1234>".to_ascii_uppercase(),
-            "<24020001,REQ,1234>".to_ascii_uppercase(),
-        ]
-        .into_iter()
-        .cycle();
+    tokio::spawn({
+        let mut path: String = format!(
+            "./shared/test-{}.txt",
+            Utc::now().format("%Y_%d_%m-%H_%M_%S")
+        );
 
-        let _name = name.clone().to_owned();
-        let port = serialport::new(_name.clone(), baudrate)
-            .timeout(Duration::from_millis(700))
-            .open()
-            .expect("Failed to Open");
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(path.clone())
+            .await?;
 
-        let mut cloned_port1 = port.try_clone().expect("Failed to clone");
-        let mut cloned_port2 = port.try_clone().expect("Failed to clone2");
+        let cloned_tx = tx_done.clone();
+        let cloned_noti = noti.clone();
+        let re = Regex::new(r"<(?<id>\d{2}).+><(?<sensors>.*)>(?<captime>.*)").unwrap();
 
-        tokio::spawn({
-            let mut path: String = format!(
-                "./shared/test-{}.txt",
-                Utc::now().format("%Y_%d_%m-%H_%M_%S")
-            );
-            let mut file = OpenOptions::new()
-                .write(true)
-                .create(true)
-                .open(path.clone())
-                .await?;
+        async move {
+            loop {
+                tokio::select! {
+                    Some(_data) = rx.recv() =>{
+                        if let Some(caps)=re.captures(&_data) {
+                            // println!("{}",&caps["sensors"]);
+                            file.write_all(format!("{} {} {}\n",&caps["id"],&caps["sensors"].split(",").collect::<Vec<&str>>()[2..8].join(" "),&caps["captime"]).as_bytes()).await.unwrap();
 
-            let cloned_tx = tx_done.clone();
-            let cloned_noti = noti.clone();
-            async move {
-                loop {
-                    tokio::select! {
-                        Some(_data) = rx.recv() =>{
-                            file.write_all(_data.as_bytes()).await.unwrap();
                         }
-                        _=cloned_noti.notified()=>{
-                            // let _path=path.clone();
-                            let _=cloned_tx.send(path.clone());
-                            path=format!("./shared/test-{}.txt", Utc::now().format("%Y_%d_%m-%H_%M_%S"));
-                            file=OpenOptions::new()
-                            .write(true)
-                            .create(true)
-                            .open(path.clone())
-                            .await.unwrap();
-                            // break
-                        }
+                    }
+                    _=cloned_noti.notified()=>{
+                        // let _path=path.clone();
+                        let _=cloned_tx.send(path.clone());
+                        path=format!("./shared/test-{}.txt", Utc::now().format("%Y_%d_%m-%H_%M_%S"));
+                        file=OpenOptions::new()
+                        .write(true)
+                        .create(true)
+                        .open(path.clone())
+                        .await.unwrap();
+                        // break
                     }
                 }
             }
-        });
-        tokio::spawn({
-            let cloned_noti = noti.clone();
-            async move {
-                loop {
-                    tokio::time::sleep(Duration::from_secs(3)).await;
-
-                    cloned_noti.notify_one();
-                }
-            }
-        });
-
-        tokio::spawn(async move {
-            let mut _tstring = tstring.clone();
-
+        }
+    });
+    tokio::spawn({
+        let cloned_noti = noti.clone();
+        async move {
             loop {
-                cloned_port2
-                    .write_all(_tstring.next().unwrap().as_bytes())
-                    .expect("Failed to write to serial port");
-                sleep(Duration::from_millis(300)).await;
+                tokio::time::sleep(Duration::from_secs(3)).await;
+                cloned_noti.notify_one();
             }
-        });
-        tokio::spawn(async move {
-            let cloned_tx = tx.clone();
-            let _ = writeln!(stdout, "{} {}", &_name, &baudrate);
+        }
+    });
 
-            let mut serial_buf: Vec<u8> = vec![0; 1000];
+    // tokio::spawn({
+    //     let cloned_tx = tx.clone();
+    //     // let cloned_tx2 = tx.clone();
+    //     // let mut rng = rand::thread_rng();
+
+    //     let mut rng = {
+    //         let rng = rand::thread_rng();
+    //         StdRng::from_rng(rng).unwrap()
+    //     };
+
+    //     async move {
+    //         let mut i = 0;
+    //         let sleep_duration = 1000 / 200;
+    //         loop {
+    //             let _ = cloned_tx.send(format!(
+    //                 "{}  {}\n",
+    //                 rng.gen::<f64>().to_string(),
+    //                 Utc::now().timestamp_millis()
+    //             ));
+    //             i = i + 1;
+    //             tokio::time::sleep(Duration::from_millis(sleep_duration)).await;
+    //         }
+    //     }
+    // });
+
+    let mut tstring = [
+        "<21020001,REQ,1234>".to_ascii_uppercase(),
+        "<22020001,REQ,1234>".to_ascii_uppercase(),
+        // "<23020001,REQ,1234>".to_ascii_uppercase(),
+    ]
+    .into_iter()
+    .cycle();
+
+    let _name = opt.port;
+    let baudrate: u32 = "115200".parse().unwrap();
+    let port = serialport::new(_name.clone(), baudrate)
+        .stop_bits(serialport::StopBits::One)
+        .timeout(Duration::from_millis(340))
+        .open()
+        .expect("Failed to Open");
+
+    let mut cloned_port1 = port.try_clone().expect("Failed to clone");
+    // let mut cloned_port2 = port.try_clone().expect("Failed to clone2");
+
+    tokio::spawn({
+        let cloned_tx = tx.clone();
+        let mut _tstring = tstring.clone();
+
+        async move {
+            let mut serial_buf: Vec<u8> = vec![0; 1024];
             println!("Receiving data on {} at {} baud:", &_name, &baudrate);
+            cloned_port1
+                .write_all(tstring.next().unwrap().as_bytes())
+                .expect("");
             loop {
                 match cloned_port1.read(serial_buf.as_mut_slice()) {
                     Ok(t) => {
-                        let _ = writeln!(
-                            stdout,
-                            "{} {}",
+                        let _ = cloned_tx.send(format!(
+                            "{}{}\n",
                             String::from_utf8_lossy(&serial_buf[..t]),
                             Utc::now().timestamp_millis()
-                        );
+                        ));
 
-                        // let _ = cloned_tx.send(format!(
-                        //     "{}\t{}\n",
-                        //     String::from_utf8_lossy(&serial_buf[..t]),
-                        //     Utc::now().timestamp_millis()
-                        // ));
+                        // sleep(Duration::from_millis(100)).await;
+
+                        cloned_port1
+                            .write_all(_tstring.next().unwrap().as_bytes())
+                            .expect("");
                     }
                     Err(ref e) if e.kind() == ErrorKind::TimedOut => {}
                     Err(e) => {
@@ -142,72 +169,47 @@ async fn main() -> anyhow::Result<()> {
                         break;
                     }
                 }
+                sleep(Duration::from_millis(10)).await;
             }
-        });
+        }
+    });
 
-        tokio::spawn({
-            async move {
-                loop {
-                    tokio::select! {
-                    Some(_path) = rx_done.recv()=>{
-                        // println!("{}",_path);
-                        let _ = listener.send_to(
-                                &_path.as_bytes(),
-                                "127.0.0.1:3132".parse::<SocketAddr>().unwrap(),
-                            ).await.unwrap();
-                        }
+    // tokio::spawn({
+    //     let mut _tstring = tstring.clone();
+    //     async move {
+    //         loop {
+    //             cloned_port2
+    //                 .write_all(_tstring.next().unwrap().as_bytes())
+    //                 .expect("Failed to write to serial port");
+    //             sleep(Duration::from_millis(200)).await;
+    //         }
+    //     }
+    // });
+
+    tokio::spawn({
+        async move {
+            loop {
+                tokio::select! {
+                Some(_path) = rx_done.recv()=>{
+                    // println!("{}",_path);
+                    let _ = listener.send_to(
+                            &_path.as_bytes(),
+                            "127.0.0.1:3132".parse::<SocketAddr>().unwrap(),
+                        ).await.unwrap();
                     }
                 }
             }
-        });
-        Ok(())
-    }
+        }
+    });
 
     tokio::task::yield_now().await;
 
     loop {
         tokio::select! {
-            evt = rl.readline().fuse() => match evt {
-                Ok(ReadlineEvent::Line(line)) => {
-                    let _command:Vec<&str> = line.split_whitespace().collect();
-
-                    match _command[0] {
-                        "ports" => {
-                            get_ports(stdout.clone());
-                        }
-                        "open_port" => {
-                            if _command.len()<2 {
-                                let _=writeln!(stdout, "Error: Arguments");
-                            } else {
-                                let _port = _command.get(1).unwrap();
-                                let _baudrate:u32 = _command.get(2).unwrap_or(&"115200").trim().parse().unwrap();
-                                open_port(stdout.clone(), cancel.clone(), _port, _baudrate).await?;
-                            }
-                        }
-                        "ble_central" =>{
-                            btlescan();
-                        }
-                        "wifi" => {
-                            startWifi();
-                        }
-                        _=>{
-                            writeln!(stdout, "").unwrap();
-                        }
-                    }
-                }
-                Ok(ReadlineEvent::Eof) => {
-                    cancel.notify_one();
-                    break
-                },
-                Ok(ReadlineEvent::Interrupted) => {
-                    cancel.notify_one();
-                    break
-                },
-                Err(e) => {
-                    writeln!(stdout, "Error: {e}")?;
-                    writeln!(stdout, "Exiting...")?;
-                    break
-                },
+            _ = signal::ctrl_c() => {
+                println!("SHUTDOWN");
+                // cancel_token.notify_one();
+                break
             }
         }
     }
